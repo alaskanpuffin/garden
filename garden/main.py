258c6c64ca44a9cf6b64fc894ecef6ccac316761
Caleb Hurley <calebhurley@protonmail.com>
2020-05-20 03:38:53 +0100

    Updates

--- garden/main.py
+++ garden/main.py
@@ -1,14 +1,25 @@
 from gpiozero import OutputDevice, Button
 from signal import pause
 
-fanButton = Button(22, hold_time=2)
+fanButton = Button(23, hold_time=2)
+waterButton = Button(24, hold_time=2)
 
-fanRelay = OutputDevice(16)
-
+fanRelay = OutputDevice(15)
+waterRelay = OutputDevice(18)
 
 def toggleFan():
+    print("Toggle Fan")
     fanRelay.toggle()
 
+def toggleWater():
+    print("Toggle Water")
+    waterRelay.toggle()
+
+def pushButton():
+   print("Toggle 2 Extra")
+
 fanButton.when_held = toggleFan
+waterButton.when_held = toggleWater
+
 
 pause()
